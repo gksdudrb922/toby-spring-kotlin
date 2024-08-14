4d5ee82adca3932dd685d1aaf78d768d200e00ea
gksdudrb922 <gksdudrb922@gmail.com>
2024-08-14 23:32:58 +0900

    1.1.3 main()을 이용한 DAO 테스트 코드
    # 1장 오브젝트와 의존관계
    ## 1.1 초난감 DAO
    ### 1.1.3 main()을 이용한 DAO 테스트 코드
    
    ✅ main 함수에 테스트 추가
    UserDao에 main 함수 만들어서 테스트해봤는데, Connection을 맺지 못해서, 스프링 부트 main 함수에 테스트 코드 추가했음.

--- src/main/kotlin/com/example/toby_spring_kotlin/TobySpringKotlinApplication.kt
+++ src/main/kotlin/com/example/toby_spring_kotlin/TobySpringKotlinApplication.kt
@@ -1,5 +1,7 @@
 package com.example.toby_spring_kotlin
 
+import com.example.toby_spring_kotlin.user.dao.UserDao
+import com.example.toby_spring_kotlin.user.domain.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +10,22 @@ class TobySpringKotlinApplication
 
 fun main(args: Array<String>) {
 	runApplication<TobySpringKotlinApplication>(*args)
+
+	val dao = UserDao()
+
+	val user = User().apply {
+		id = "1"
+		name = "han"
+		password = "1234"
+	}
+
+	dao.add(user)
+
+	println("${user.id} 등록 성공")
+
+	val user2 = dao.get(user.id!!)
+	println(user2.name)
+	println(user2.password)
+
+	println("${user2.id} 조회 성공")
 }
