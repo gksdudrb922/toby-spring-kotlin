c1cde497106bded3950165c2c367d2f0eee44404
gksdudrb922 <gksdudrb922@gmail.com>
2024-08-18 23:13:11 +0900

    2.2.2 테스트의 효율적인 수행과 결과 관리
    # 2장 테스트
    ## 2.2 UserDaoTest 개선
    ### 2.2.2 테스트의 효율적인 수행과 결과 관리
    
    ✅ JUnit 테스트로 전환
    기존 main 함수에서 수행하던 테스트를 JUnit 테스트로 변경
    +) 주의: 한번 테스트를 성공하고, 다시 테스트를 실행하면 PK 중복으로 에러 발생 -> 다음 커밋에서 테스트마다 매번 데이터를 삭제해주는 코드를 추가할 예정.

--- src/main/kotlin/com/example/toby_spring_kotlin/TobySpringKotlinApplication.kt
+++ src/main/kotlin/com/example/toby_spring_kotlin/TobySpringKotlinApplication.kt
@@ -1,75 +1,11 @@
 package com.example.toby_spring_kotlin
 
-import com.example.toby_spring_kotlin.config.CountingDaoFactory
-import com.example.toby_spring_kotlin.config.DaoFactory
-import com.example.toby_spring_kotlin.infra.CountingDataSource
-import com.example.toby_spring_kotlin.user.dao.UserDao
-import com.example.toby_spring_kotlin.user.domain.User
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 @SpringBootApplication
 class TobySpringKotlinApplication
 
 fun main(args: Array<String>) {
 	runApplication<TobySpringKotlinApplication>(*args)
-
-	userDaoTest()
-	userDaoCountingTest()
-}
-
-private fun userDaoTest() {
-	val context = AnnotationConfigApplicationContext(DaoFactory::class.java)
-	val dao = context.getBean("userDao", UserDao::class.java)
-
-	val user = User().apply {
-		id = "1"
-		name = "han"
-		password = "1234"
-	}
-
-	dao.add(user)
-
-	println("${user.id} 등록 성공")
-
-	val user2 = dao.get(user.id!!)
-	if (user.name != user2.name) {
-		println("테스트 실패 (name)")
-	} else if (user.password != user2.password) {
-		println("테스트 실패 (password)")
-	} else {
-		println("조회 테스트 성공")
-	}
-}
-
-private fun userDaoCountingTest() {
-	val context = AnnotationConfigApplicationContext(CountingDaoFactory::class.java)
-	val dao = context.getBean("userDaoCounting", UserDao::class.java)
-
-	val user = User().apply {
-		id = "2"
-		name = "han"
-		password = "1234"
-	}
-
-	dao.add(user)
-
-	println("${user.id} 등록 성공")
-
-	val user2 = dao.get(user.id!!)
-	if (user.name != user2.name) {
-		println("테스트 실패 (name)")
-	} else if (user.password != user2.password) {
-		println("테스트 실패 (password)")
-	} else {
-		println("조회 테스트 성공")
-	}
-
-	val ccm = context.getBean("dataSourceCounting", CountingDataSource::class.java)
-	if (ccm.counter != 2) {
-		println("테스트 실패 (counter)")
-	} else {
-		println("카운터 테스트 성공")
-	}
 }
---
+++ src/test/kotlin/com/example/toby_spring_kotlin/user/dao/UserDaoTest.kt
@@ -0,0 +1,52 @@
+package com.example.toby_spring_kotlin.user.dao
+
+import com.example.toby_spring_kotlin.config.CountingDaoFactory
+import com.example.toby_spring_kotlin.config.DaoFactory
+import com.example.toby_spring_kotlin.infra.CountingDataSource
+import com.example.toby_spring_kotlin.user.domain.User
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class UserDaoTest {
+
+    @Test
+    fun addAndGet() {
+        val context = AnnotationConfigApplicationContext(DaoFactory::class.java)
+        val dao = context.getBean("userDao", UserDao::class.java)
+
+        val user = User().apply {
+            id = "1"
+            name = "han"
+            password = "1234"
+        }
+
+        dao.add(user)
+
+        val user2 = dao.get(user.id!!)
+        assertEquals(user.name, user2.name)
+        assertEquals(user.password, user2.password)
+    }
+
+    @Test
+    fun addAndGetAndCounter() {
+        val context = AnnotationConfigApplicationContext(CountingDaoFactory::class.java)
+        val dao = context.getBean("userDaoCounting", UserDao::class.java)
+
+        val user = User().apply {
+            id = "2"
+            name = "han"
+            password = "1234"
+        }
+
+        dao.add(user)
+
+        val user2 = dao.get(user.id!!)
+        assertEquals(user.name, user2.name)
+        assertEquals(user.password, user2.password)
+
+        val ccm = context.getBean("dataSourceCounting", CountingDataSource::class.java)
+        assertEquals(2, ccm.counter)
+    }
+
+}
